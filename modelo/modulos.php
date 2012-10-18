8a72fff44fd9854a909c41915a21ed20fbb34b38
ever <sonico999@gmail.com>
2012-10-18 00:54:51 -0500

    agregar las clases perfiles y modulos

---
+++ modelo/modulos.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of modulos
+ *
+ * @author pedro
+ */
+class modulos {
+    //put your code here
+}
+
+?>
