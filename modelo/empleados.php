9675428172e6d73a1033210fee186b8bf5af19f8
ever <sonico999@gmail.com>
2012-10-18 00:23:11 -0500

    Inicializamos el proyecto

---
+++ modelo/empleados.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of empleados
+ *
+ * @author pedro
+ */
+class empleados {
+    //put your code here
+}
+
+?>
