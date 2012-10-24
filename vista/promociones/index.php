3260f06229e142f7dd730b77710e9f27de285e1b
ja1r <jair_vr_10@hotmail.com>
2012-10-24 18:46:11 -0500

    ayeee

--- vista/promociones/index.php
+++ vista/promociones/index.php
@@ -1,6 +1,6 @@
 
 	<h3>Lista de Promociones</h3>
-    <table border="1">
+   <!-- <table border="1">
         <tr>
             <td>Codigo</td>
             <td>Descripcion</td>
@@ -8,5 +8,60 @@
             <td>Fecha de Inicio</td>
             <td>Fecha de Finalizacion</td>
         </tr>
-    </table>
+    </table>-->
     <a href="nuevo" class="k-button">Nuevo</a>
+<div id="grid"></div>
+<script>
+                $(document).ready(function () {
+                    var crudServiceBaseUrl = "http://demos.kendoui.com/service",
+                        dataSource = new kendo.data.DataSource({
+                            transport: {
+                                read:  {
+                                    url: crudServiceBaseUrl + "/Products"
+                                },
+                                update: {
+                                    url: crudServiceBaseUrl + "/Products/Update"
+                                },
+                                destroy: {
+                                    url: crudServiceBaseUrl + "/Products/Destroy"
+                                },
+                                create: {
+                                    url: "nuevo",
+                                    dataType: "jsonp"
+                                },
+                                parameterMap: function(options, operation) {
+                                    if (operation !== "read" && options.models) {
+                                        return {models: kendo.stringify(options.models)};
+                                    }
+                                }
+                            },
+                            batch: true,
+                            pageSize: 8,
+                            schema: {
+                                model: {
+                                    id: "idpromocion",
+                                    fields: {
+                                        idpromocion: { editable: false, nullable: true },
+                                        descripcion: { validation: { required: true } },
+                                        descuento: { type: "number", validation: { required: true, min: 1} },
+                                        fecha_inicio: { type: "boolean" },
+                                        fecha_final: { type: "number", validation: { min: 0, required: true } }
+                                    }
+                                }
+                            }
+                        });
+
+                    $("#grid").kendoGrid({
+                        dataSource: dataSource,
+                        pageable: true,
+                        height: 380,
+                        columns: [
+                            { field:"descripcion", title: "Promocion" },
+                            { field: "descuento", title:"Descuento", format: "{0:c}", width: "130px" },
+                            { field: "fecha_inicio", title:"Fecha de Inicio", width: "130px" },
+                            { field: "fecha_final", title:"Fecha Final", width: "130px" },
+                            { command: ["edit", "destroy"], title: "&nbsp;", width: "210px" }],
+                        editable: "popup"
+                    });
+                });
+            </script>
