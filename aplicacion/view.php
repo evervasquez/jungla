28b9311db7acee0c594da4cf27b6b0a53783cef1
ja1r <jair_vr_10@hotmail.com>
2012-11-07 10:16:12 -0500

    aaaa

--- aplicacion/view.php
+++ aplicacion/view.php
@@ -15,12 +15,14 @@ class view {
     private $_controlador;
     private $_menu;
     private $_js;
+    private $_css;
     //parametro request = es el parametro del ccontrolador
     public function __construct(request $peticion, $menu) {
         //guardamos el nombre del controlador
         $this->_controlador = $peticion->get_controlador();
         $this->_menu=$menu;
         $this->_js = array();
+        $this->_css = array();
     }
 
     public function renderizar($vista, $item = false) {
@@ -30,13 +32,21 @@ class view {
         $ruta_vista = ROOT . 'vista' . DS . $this->_controlador . DS . $vista.'.php';
         
         $js = array();
+        $css = array();
         
         if(count($this->_js)){
             $js = $this->_js;
         }
+        if(count($this->_css)){
+            $css = $this->_css;
+        }
         
         $_params = array(
-            'js' => $js
+            'ruta_css' => BASE_URL . 'lib/css/',
+            'ruta_js' => BASE_URL . 'lib/js/',
+            'ruta_img' => BASE_URL . 'lib/img/',
+            'js' => $js,
+            'css' => $css
         );
         
         //die($ruta_vista);
@@ -45,13 +55,6 @@ class view {
         if (is_readable($ruta_vista)) {
             //enviamos parametros como css, js
             //archivos propios del template
-            /* $_layoutParams= array(
-              'ruta_css'=> BASE_URL.'vistas/layout/'.DEFAULT_LAYOUT.'/css/',
-              'ruta_img'=> BASE_URL.'vistas/layout/'.DEFAULT_LAYOUT.'/img/',
-              'ruta_js'=> BASE_URL.'vistas/layout/'.DEFAULT_LAYOUT.'/js/',
-              'menu'=> $menu
-              ); */
-
             //incluimos los layout
             include_once ROOT . DS . 'cabecera.php';
             include_once ROOT. DS . 'menu.php';
@@ -97,10 +100,21 @@ class view {
     {
         if(is_array($js) && count($js)){
             for($i=0; $i < count($js); $i++){
-                $this->_js[] = BASE_URL . 'vista' . DS . $this->_controlador . DS . "js" . DS . $js[$i] . '.js';
+                $this->_js[] = BASE_URL . 'vista/' . $this->_controlador . "/js/" . $js[$i] . '.js';
             }
         } else {
             throw new Exception('Error de js');
+        }
+    }
+    
+    public function setCss(array $css)
+    {
+        if(is_array($css) && count($css)){
+            for($i=0; $i < count($css); $i++){
+                $this->_css[] = BASE_URL . 'vista/' . $this->_controlador . "/css/" . $css[$i] . '.css';
+            }
+        } else {
+            throw new Exception('Error de css');
         }
     }
 
