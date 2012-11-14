66b4fcf501d2f6647a9ad499f10a11b791805aa3
medalol <mauro_1142@hotmail.es>
2012-11-13 19:08:35 -0500

    nuevo

--- modelo/producto_paquete.php
+++ modelo/producto_paquete.php
@@ -7,7 +7,13 @@ class producto_paquete {
     public $cantidad;
 
     public function selecciona() {
-        $datos = array($this->idalmacen, $this->idproducto);
+        if(is_null($this->idpaquete)){
+            $this->idpaquete=0;
+        }
+        if(is_null($this->idproducto)){
+            $this->idproducto=0;
+        }
+        $datos = array($this->idpaquete, $this->idproducto);
         $r = consulta::procedimientoAlmacenado("pa_selecciona_producto_paquete", $datos);
         if ($r[1] == '') {
             $stmt = $r[0];
@@ -15,11 +21,11 @@ class producto_paquete {
             die($r[1]);
         }
         $r = null;
-        return $stmt->fetchall();
+        return $stmt->fetchall(PDO::FETCH_ASSOC);
     }
 
     public function elimina() {
-        $datos = array($this->idalmacen, $this->idproducto);
+        $datos = array($this->idpaquete, $this->idproducto);
         $r = consulta::procedimientoAlmacenado("pa_elimina_producto_paquete", $datos);
         $error = $r[1];
         $r = null;
@@ -27,7 +33,7 @@ class producto_paquete {
     }
 
     public function inserta() {
-        $datos = array($this->idalmacen, $this->idproducto, $this->cantidad);
+        $datos = array($this->idpaquete, $this->idproducto, $this->cantidad);
         $r = consulta::procedimientoAlmacenado("pa_inserta_actualiza_producto_paquete", $datos);
         $error = $r[1];
         $r = null;
@@ -35,7 +41,7 @@ class producto_paquete {
     }
 
     public function actualiza() {
-        $datos = array($this->idalmacen, $this->idproducto, $this->cantidad);
+        $datos = array($this->idpaquete, $this->idproducto, $this->cantidad);
         $r = consulta::procedimientoAlmacenado("pa_inserta_actualiza_producto_paquete", $datos);
         $error = $r[1];
         $r = null;
