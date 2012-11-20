1287a2e608d14c0e60d169f5f1f520918af89cdc
medalol <mauro_1142@hotmail.es>
2012-11-20 15:52:08 -0500

    otro

--- vista/entrada_productos/confirmacion.php
+++ vista/entrada_productos/confirmacion.php
@@ -1,6 +1,6 @@
 <h3>Entradas pendientes:</h3>
 <label>Compra:</label>
-<select class="list" placeholder="Seleccione...">
+<select class="list" placeholder="Seleccione..." id="compra">
     <option></option>
     <?php for($i=0;$i<count($this->datos);$i++){ ?>
         <?php if($this->datos[$i]['confirmacion'] == 0){ ?>
@@ -8,3 +8,4 @@
         <?php } ?>
     <?php } ?>
 </select>
+<p><div id="infocompra"></div></p>
