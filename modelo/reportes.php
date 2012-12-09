91709b4055729beb0c068ad498e12ccb09581749
sonico999 <sonico999@gmail.com>
2012-12-09 17:04:13 -0500

    fecha en controlador caja

--- modelo/reportes.php
+++ modelo/reportes.php
@@ -93,7 +93,6 @@ class reportes extends Main{
         $datos=$mesano;
         $r = $this->get_consulta("pa_selecciona_numero_arribos_x_tipo_habitacion", $datos);
         if ($r[1] == '') {$stmt = $r[0];} else {die($r[1]);}$r = null;
-        return $stmt->fetchall(PDO::FETCH_ASSOC);
         if (BaseDatos::$_archivo == 'OCI') {
             oci_fetch_all($stmt, $data, null, null, OCI_FETCHSTATEMENT_BY_ROW);
             return $data;
