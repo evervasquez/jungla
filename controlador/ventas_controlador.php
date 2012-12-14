6c017cd8761fbac255c74c1d8edd05def0d9b969
ja1r <jair_vr_10@hotmail.com>
2012-12-13 21:44:30 -0500

    modif

--- controlador/ventas_controlador.php
+++ controlador/ventas_controlador.php
@@ -129,7 +129,7 @@ class ventas_controlador extends controller {
                 $fecha_temp = date("d-m-Y", strtotime("$fecha_venta +$intervalo_dias day"));
 
                 for($i=1;$i<=$c;$i++){
-                    $this->_cuota_cobro->idventa=$dato_venta[0]['IDVENTA'];
+                    $this->_cuota_cobro->idventa=$dato_venta[0]['X_IDVENTA'];
                     $this->_cuota_cobro->fecha_cobro=$fecha_temp;
                     $this->_cuota_cobro->nro_cobro=$i;
                     $this->_cuota_cobro->monto_cuota=$cuota[$i];
@@ -137,7 +137,7 @@ class ventas_controlador extends controller {
                     $fecha_temp = date("d-m-Y", strtotime("$fecha_temp +$intervalo_dias day"));
                 }
             }
-            $this->_asientos->idventa=$dato_venta[0]['IDVENTA'];
+            $this->_asientos->idventa=$dato_venta[0]['X_IDVENTA'];
             $this->_asientos->inserta();
             $this->redireccionar('ventas');
         }
